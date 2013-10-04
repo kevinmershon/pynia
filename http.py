586a625244d893e1500ed78efbc1971b83e0aa9b
Kevin Mershon <nwlinkvxd@gmail.com>
2013-10-04 15:11:20 -0700

    Add background updater to poll data

--- http.py
+++ http.py
@@ -1,14 +1,61 @@
+import sys
+import threading
 import web
+import nia as NIA
 
 urls = (
     '/', 'index'
 )
+
+# global scope stuff
+nia = None
+nia_data = None
+updater = None
+
+brain_fingers = None
 
 class index:
     def GET(self):
         render = web.template.render("templates/")
         return render.index()
 
+class Updater:
+    def update(self):
+        while True:
+            # kick-off processing data from the NIA
+            data_thread = threading.Thread(target=nia_data.get_data)
+            data_thread.start()
+
+            # get the fourier data from the NIA
+            data, brain_fingers = nia_data.fourier(nia_data)
+
+            # wait for the next batch of data to come in
+            data_thread.join()
+
+            # exit if we cannot read data from the device
+            if nia_data.AccessDeniedError:
+                sys.exit(1)
+
 if __name__ == "__main__":
     app = web.application(urls, globals())
+
+    # open the NIA, or exit with a failure code
+    nia = NIA.NIA()
+    if not nia.open():
+        sys.exit(1)
+
+    # start collecting data
+    milliseconds = 50
+    nia_data = NIA.NiaData(nia, milliseconds)
+
+    # kick-off processing data from the NIA
+    updater = Updater()
+    update_thread = threading.Thread(target=updater.update)
+    update_thread.start()
+
+    # run the app
     app.run()
+
+    # when web.py exits, close out the NIA and exit gracefully
+    nia.close()
+    sys.exit(0)
